c2572b9171b2269396e5e08731d43a1d0adfa084
Pin-Chyan <50664297+Pin-Chyan@users.noreply.github.com>
2020-08-12 15:56:23 +0200

    java funds

---
+++ MyClass.java
@@ -0,0 +1,163 @@
+public class MyClass {
+	public static void main(String[] args) {
+
+
+		//----------------------------------------------------------------------------------------------------
+
+		//System.out.println("Part 6");
+
+		//String greeting = "Hello";
+		//System.out.println(greeting);
+
+		//String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		//System.out.println("The length of the txt string is: " + txt.length());
+
+		//String Cases = "Hello World";
+		//System.out.println(Cases.toUpperCase());
+		//System.out.println(Cases.toLowerCase());
+
+		//String index = "Please locate where 'locate' occurs!";
+		//System.out.println(index.indexOf("locate"));
+		
+		//String firstName = "John";
+		//String lastName = "Doe";
+		//System.out.println(firstName + " " + lastName);
+		//System.out.println(firstName.concat(lastName));
+
+		//String message = "We are the so-called \"Vikings\" from the north.";
+		//System.out.println(message);
+
+		//int ix = 10;
+		//int iy = 20;
+		//int iz = ix + iy;
+		//System.out.println(iz);
+
+		//String sx = "10";
+		//String sy = "20";
+		//String sz = sx + sy;
+		//System.out.print(sz);
+
+
+		//----------------------------------------------------------------------------------------------------
+
+
+
+		//System.out.println("Part 5");
+
+		//int x = 100 + 50;
+		//int y = 10;
+		//y += 5;
+		//System.out.println(x);
+		//System.out.println(y);
+
+		//int sum1 = 100 + 50;
+		//int sum2 = sum1 + 250;
+		//int sum3 = sum2 + sum2;
+		//System.out.println(sum1);
+		//System.out.println(sum2);
+		//System.out.println(sum3);		
+
+
+		//----------------------------------------------------------------------------------------------------
+
+
+		//System.out.println("Part 4");
+
+
+		//int myInt = 9;
+		//double myDouble = myInt;
+
+		//System.out.println(myInt);
+		//System.out.println(myDouble);
+	
+
+		//double tdouble = 9.78;
+		//int tnum = (int)tdouble;
+		//System.out.println(tdouble);
+		//System.out.println(tnum);
+
+
+		//----------------------------------------------------------------------------------------------------
+
+
+		//System.out.println("Part 3");
+
+		//int myNum = 5;               // Integer (whole number)
+		//float myFloatNum = 5.99f;    // Floating point number
+		//char myLetter = 'D';         // Character
+		//boolean myBool = true;       // Boolean
+		//String myText = "Hello";     // String
+
+		//byte bnum = 100;
+		//System.out.println(bnum);
+
+		//short snum = 5000;
+		//System.out.println(snum);
+
+		//int inum = 10000;
+		//System.out.println(inum);
+
+		//long lnum = 150000000L; // use L for ending longs
+		//System.out.println(lnum);
+
+		//float fnum = 5.75f; // use of f for ending floats
+		//System.out.println(fnum);
+
+		//double dnum = 19.99d; // use of d for ending doubles
+		//System.out.println(dnum);
+
+		//float f1 = 35e3f;
+		//double d1 = 12E4d;
+		//System.out.println(f1);
+		//System.out.println(d1);
+
+		//boolean isJavaFun = true;
+		//boolean isFishTasty = false;
+		//System.out.println(isJavaFun);
+		//System.out.println(isFishTasty);
+
+		//char myGrade = 'B';
+		//System.out.println(myGrade);
+
+		//char a = 65, b = 66, c = 65;
+		//System.out.println(a);
+		//System.out.println(b);
+		//System.out.println(c);
+
+		//String greeting = "Hello World";
+		//System.out.println(greeting);
+
+
+		//----------------------------------------------------------------------------------------------------
+
+		//System.out.println("Part 2");
+
+		//basics of java // variables strings
+		//String name = "John ";
+		//String surname = "Doe";
+		//String fullname = name + surname;
+		//System.out.println(fullname);
+
+		//basic of java // variables numbers
+		//int x = 5;
+		//int y = 6;
+		//System.out.println(x + y);
+
+		//basics of java // multiple variables
+		//int x = 5, y = 6, z = 50;
+		//System.out.println(x + y + z);
+
+		//----------------------------------------------------------------------------------------------------
+
+		//System.out.println("Part 1");
+
+		//basics of java // sending your own text and variables
+		//String name = "John";
+		//System.out.println("Hello " + name);
+
+		//basics of java // displaying number variables
+		//final int num; 
+		//num = 15;
+		//System.out.println(num);
+	}
+}
---
+++ MyClass2.java
@@ -0,0 +1,41 @@
+public class MyClass2 {
+	public static void main(String[] args) {
+
+		//----------------------------------------------------------------------------------------------------
+
+		System.out.println("Part 9");
+
+
+
+
+		//----------------------------------------------------------------------------------------------------
+		
+
+		//System.out.println("Part 8");
+
+		//int x = 10;
+		//int y = 9;
+		//System.out.println(x > y);
+		//System.out.println(x == 10);
+		//System.out.println(x == 15);
+
+
+
+		//----------------------------------------------------------------------------------------------------
+
+
+		//System.out.println("Part 7");
+
+		//System.out.println(Math.max(5,10));
+		//System.out.println(Math.min(5,10));
+		//System.out.println(Math.sqrt(64));
+		//System.out.println(Math.abs(-4.7));
+		//System.out.println(Math.random());
+		
+		//int randomNum = (int)(Math.random() * 101);
+		//System.out.println(randomNum);
+
+
+
+	}
+}
